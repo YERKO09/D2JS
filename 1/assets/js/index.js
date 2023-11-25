e09645ae86b2a6744417da382b328f206df60870
Yerko Browton <yerko.browton1995@hotmail.com>
2023-11-25 20:25:13 -0300

    Cambio de propiedad border por outline

--- 1/assets/js/index.js
+++ 1/assets/js/index.js
@@ -4,12 +4,12 @@ let bordeActivo = false;
 function bordeImg() {
 
     if (bordeActivo == false) {
-        imagen.style.border = "solid #5CFF45 5px"
+        imagen.style.outline = "solid #5CFF45 5px"
         imagen.style.transition = ".5s ease"
         bordeActivo = true
     }
     else {
-        imagen.style.border = "solid #0C6200 0px"
+        imagen.style.outline = "solid #0C6200 0px"
         bordeActivo = false
     }
 }
